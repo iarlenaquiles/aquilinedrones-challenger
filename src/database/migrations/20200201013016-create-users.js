bc73035181c3f9de96fb5ad78ecfd22fcf5d263c
Iarlen Aquiles <iarlensilva775@gmail.com>
2020-08-21 14:53:45 -0300

    criando migration do user

---
+++ src/database/migrations/20200201013016-create-users.js
@@ -0,0 +1,43 @@
+"use strict";
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("Users", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      deletedAt: {
+        type: Sequelize.DATE,
+        allowNull: true
+      }
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable("Users");
+  }
+};
