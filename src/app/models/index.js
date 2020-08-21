7045d09f743533089f721935f4d00f14fe087ce4
Iarlen Aquiles <iarlensilva775@gmail.com>
2020-08-21 14:51:45 -0300

    criando models de usuarios

---
+++ src/app/models/index.js
@@ -0,0 +1,58 @@
+"use strict";
+
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
+const basename = path.basename(__filename);
+const config = require("../../config/database");
+const db = {};
+const mysql = require("mysql2/promise");
+
+mysql
+  .createConnection({
+    user: config.username,
+    password: config.password
+  })
+  .then(connection => {
+    connection
+      .query(`CREATE DATABASE IF NOT EXISTS ${config.database};`)
+      .then(() => {});
+  });
+
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Conectado com sucesso");
+  })
+  .catch(erro => {
+    console.log("Falha ao se conectar:" + erro);
+  });
+
+fs.readdirSync(__dirname)
+  .filter(file => {
+    return (
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+    );
+  })
+  .forEach(file => {
+    const model = sequelize["import"](path.join(__dirname, file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
